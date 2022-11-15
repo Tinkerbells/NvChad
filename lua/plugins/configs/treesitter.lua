7fdc9d38568e9c6df8aa8f9c6abe6051aec18ee4
Unter Mensch <untermenschh@protonmail.com>
2022-11-15 19:59:27 +0300

    add treesitter ensure installed providers

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -9,6 +9,10 @@ require("base46").load_highlight "treesitter"
 local options = {
   ensure_installed = {
     "lua",
+    "typescript",
+    "tsx",
+    "javascript",
+    "css",
   },
 
   highlight = {
