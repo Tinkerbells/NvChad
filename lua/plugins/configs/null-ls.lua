3413cfd1f8ca1b4b6899d35207022ef5c225d6ff
Unter Mensch <untermenschh@protonmail.com>
2022-11-15 19:58:22 +0300

    add null-ls formatting config

---
+++ lua/plugins/configs/null-ls.lua
@@ -0,0 +1,29 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- b.formatting.prettierd (fast version of prettier),
+  b.formatting.prettierd.with {
+    extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+  },
+  -- Lua
+  b.formatting.stylua,
+
+  -- Shell
+  b.formatting.beautysh,
+  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+
+  -- cpp
+  b.formatting.clang_format,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
